5d3dc62fb5c35125a008b8992873183f0edbf6d8
mayur7267 <mayurlate.7267@gmail.com>
2025-04-23 23:49:18 +0530

    Initial Commit

---
+++ Task11/Task11App.swift
@@ -0,0 +1,17 @@
+//
+//  Task11App.swift
+//  Task11
+//
+//  Created by Mayur on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Task11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Task11Tests/Task11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Task11Tests.swift
+//  Task11Tests
+//
+//  Created by Mayur on 23/04/25.
+//
+
+import Testing
+@testable import Task11
+
+struct Task11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
